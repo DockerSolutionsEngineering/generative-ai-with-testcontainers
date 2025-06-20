817508df70dc01ff9552dec46582372a53be8185
Anna Chernyshova <anna.chernyshova@docker.com>
2025-06-20 14:53:12 -0400

    Added mocks response

--- 6-testing/src/test/java/com/example/generativeai/testcontainers/OpenAIServiceMicrocksTest.java
+++ 6-testing/src/test/java/com/example/generativeai/testcontainers/OpenAIServiceMicrocksTest.java
@@ -1,19 +1,30 @@
 package com.example.generativeai.testcontainers;
 
+import com.example.generativeai.testcontainers.agents.ValidatorAgent;
+import dev.langchain4j.service.AiServices;
 import io.github.microcks.testcontainers.MicrocksContainer;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 
+import static com.example.generativeai.testcontainers.Base.chatModel;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
 @Testcontainers
+@Slf4j
 class OpenAIServiceMicrocksTest {
     static String microcksUrl;
     static String question = "In F1 2024 season which driver won the Great Britain Grand Prix?";
+
+    static ValidatorAgent validatorAgent;
+    final static String reference = """
+            - Answer must indicate that Lewis Hamilton won the Great Britain Grand Prix.
+            """;
 
     @Container
     static MicrocksContainer microcks = new MicrocksContainer("quay.io/microcks/microcks-uber:latest")
@@ -22,19 +33,22 @@ class OpenAIServiceMicrocksTest {
     @BeforeAll
     static void setUp() {
         microcksUrl = microcks.getRestMockEndpoint("OpenAI API", "1.0");
-    }
-
-    @Test
-    void testChatCompletion() {
-        GetAnswerAgent getAnswerAgent = new GetAnswerAgent(microcksUrl, "gpt-3.5-turbo");
-        String response = getAnswerAgent.getStraightAnswer(question);
-        assertTrue(response.contains("Verstappen"), "Response should contain 'assistant' but was: " + response);
+        String baseValidatorUrl = "http://localhost:12434/engines/llama.cpp/v1";
+        String validatorModelName = "ai/gemma3";
+        validatorAgent = AiServices.builder(ValidatorAgent.class)
+                .chatLanguageModel(chatModel(validatorModelName, baseValidatorUrl))
+                .build();
     }
 
     @Test
     void testModelHallucinate() {
-        GetAnswerAgent getAnswerAgent = new GetAnswerAgent(microcksUrl, "hallucinate");
-        String response = getAnswerAgent.getStraightAnswer(question);
-        assertTrue(response.contains("Leclerc"), "... was: " + response);
+        GetAnswerAgent getAnswerAgentAgent = new GetAnswerAgent(microcksUrl, "hallucinate");
+        String response = getAnswerAgentAgent.getStraightAnswer(question);
+        log.info("Straight Answer: {}", response);
+
+        ValidatorAgent.ValidatorResponse validate = validatorAgent.validate(question, response, reference);
+        log.info("Validation: {}", validate);
+
+        assertEquals("yes", validate.response());
     }
 }
